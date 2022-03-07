822504f02f24e5a50eb7b04970c2eeae21cd5d39
lange <lange@synyx.de>
2022-03-07 15:43:08 +0100

    added long-running task

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -13,5 +15,17 @@ public class IndexController {
         model.addAttribute("data", "1234 567  as");
 
         return "index";
+    }
+
+
+    @GetMapping("longRunning")
+    public String longRunning(Model model) throws InterruptedException {
+        for (int i=0; i<90; ++i) {
+            Thread.sleep(1000);
+            log.info("waited {} seconds", i);
+        }
+        log.info("finished");
+
+        return "longRunning";
     }
 }
