b7531289df7eea838bca3af6e1cda95628ee017e
lange <lange@synyx.de>
2022-03-07 16:21:29 +0100

    StreamingResponseExample with html-return

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
@@ -57,6 +60,27 @@ public class IndexController {
             }
         });
         return emitter;
+    }
+
+    @GetMapping("/srb")
+    public ResponseEntity<StreamingResponseBody> handleRbe() {
+        StreamingResponseBody stream = out -> {
+            try {
+                for (int i = 0; i < 20; ++i) {
+                    String msg = "<!-- update: %s -->".formatted(new Date().toString());
+                    out.write(msg.getBytes());
+                    out.flush();
+                    Thread.sleep(1000);
+                    log.info("srb - waited {} seconds", i);
+                }
+            } catch (Exception ex) {
+                log.error("error in StreamingResponseBody", ex);
+            }
+            log.info("srb - finished");
+            String msg = "<html><body><h1>hello world</body></html>";
+            out.write(msg.getBytes());
+        };
+        return new ResponseEntity<>(stream, HttpStatus.OK);
     }
 
 }
