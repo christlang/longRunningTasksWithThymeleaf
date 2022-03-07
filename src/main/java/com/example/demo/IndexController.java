c28643a085e9c844044b0d0947d626f127ea40b8
lange <lange@synyx.de>
2022-03-07 15:26:13 +0100

    use docker and nginx

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
 
-        model.addAttribute("data", "abcdfe");
+        model.addAttribute("data", "1234 567  as");
 
         return "index";
     }
