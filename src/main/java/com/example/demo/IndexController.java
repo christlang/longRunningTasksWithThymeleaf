19fa239b0e9e9993e70ad39bda4da81a58fff6a2
lange <lange@synyx.de>
2022-03-07 16:11:36 +0100

    added sse example

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Controller
 public class IndexController {
+
+    private final ExecutorService nonBlockingService = Executors.newCachedThreadPool();
 
     @GetMapping("/")
     public String index(Model model) {
@@ -22,10 +30,33 @@ public class IndexController {
     public String longRunning(Model model) throws InterruptedException {
         for (int i=0; i<10; ++i) {
             Thread.sleep(1000);
-            log.info("waited {} seconds", i);
+            log.info("longRunning - waited {} seconds", i);
         }
-        log.info("finished");
+        log.info("longRunning - finished");
 
         return "longRunning";
     }
+
+    @GetMapping("/sse")
+    public SseEmitter handleSse() {
+        SseEmitter emitter = new SseEmitter();
+        nonBlockingService.execute(() -> {
+            try {
+                for (int i = 0; i < 10; ++i) {
+                    emitter.send("<!-- update: %s -->".formatted(new Date().toString()));
+                    Thread.sleep(1000);
+                    log.info("sse - waited {} seconds", i);
+                }
+                emitter.send("<html><body><h1>Hello world</body></html>", MediaType.TEXT_HTML);
+                log.info("sse - finished");
+
+                // we could send more events
+                emitter.complete();
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        });
+        return emitter;
+    }
+
 }
